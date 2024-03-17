f36793445454d00e9e6115856d663d8f42c34e38
Alexandre <alepranskunas@hotmail.com>
2024-03-17 17:35:30 -0300

    conectionDb

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
-  module.exports = {mysqlURI: "mysql://manutencao:Navigu_03@awseb-e-3px43ti4i9-stack-awsebrdsdatabase-o542scrhyptb.cuuomrcedmbe.sa-east-1.rds.amazonaws.com:3306/ebdb"}
+  module.exports = {mysqlURI: "mysql://sql10692176:VmARu11efc@sql10.freesqldatabase.com:3306/sql10692176"}
 }else {
   module.exports = {mysqlURI: "mysql://cosmeticos:pransk@localhost:3306/cosmeticos"}
 }
