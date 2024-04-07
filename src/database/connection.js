3b03dee648234d962b08d7058c3c9f8f3646f512
Alexandre <alepranskunas@hotmail.com>
2024-04-06 21:35:12 -0300

    novo banco

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
-  module.exports = {mysqlURI: "mysql://sql10692176:VmARu11efc@sql10.freesqldatabase.com:3306/sql10692176"}
+  module.exports = {mysqlURI: "mysql://sql10697263:wMAGUcvarI@sql10.freesqldatabase.com:3306/sql10697263"}
 }else {
   module.exports = {mysqlURI: "mysql://cosmeticos:pransk@localhost:3306/cosmeticos"}
 }
